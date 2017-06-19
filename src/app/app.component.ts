1a1f8d38461d0bc085c2e2dabbc50ce0bfd021bd
David Olumbe <davidolumbe@Development.Quartix.local>
2017-06-19 08:54:11 +0100

    add loading function on login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {LoadingController, Platform} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {AuthProvider} from "../providers/auth/auth";
@@ -11,28 +10,12 @@ import {WelcomePage} from "../pages/welcome/welcome";
 })
 export class MyApp {
   rootPage: any = WelcomePage;
-  loader: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
-              public loadingCtrl: LoadingController,
-              public authProvider: AuthProvider) {
+  constructor(pstatusBar: StatusBar, splashScreen: SplashScreen, public authProvider: AuthProvider) {
 
-   // this.presentLoading();
-   //  this.authProvider.login().then((isLoggedIn) => {
-   //    if (isLoggedIn) {
-   //      this.rootPage = HomePage;
-   //    } else {
-   //      this.rootPage = LoginPage;
-   //    }
-   //    this.loader.dismiss();
-   //  });
+
   }
 
-  presentLoading() {
-    this.loader = this.loadingCtrl.create({
-      content: "Authentication",
-    });
-    this.loader.present();
-  }
+
 }
 
