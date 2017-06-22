dec1ae0baeee58488455e30106afd0093ee80aec
David Olumbe <info@visilinx.com>
2017-06-22 22:02:26 +0100

    display guest info

--- src/pages/guest/guest.ts
+++ src/pages/guest/guest.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the GuestPage page.
@@ -14,16 +14,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GuestPage {
 
+  private guestName: string = '';
+  private userId: string = '';
+  private visiting: string = '';
+  private address: string = '';
+  private arrivalDate: string = '';
+  private leavingDate: string = '';
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GuestPage ' + JSON.stringify(this.navParams.get('data')));
+    let data = this.navParams.get('data');
+    this.guestName = data[0].VisitorName;
+    this.userId = data[0].UserId;
+    this.visiting = data[0].Visiting;
+    this.address = data[0].Address;
+    this.arrivalDate = data[0].ArrivalDate;
+    this.leavingDate = data[0].LeavingDate;
+
 
     this.displayGuestInfo();
   }
 
-  displayGuestInfo(){
+  displayGuestInfo() {
 
   }
 
